bf6327b79bf0beb14e0b3ffed9b8905a47445629
captain-pool <rickdey1998@gmail.com>
2018-12-23 00:50:42 +0530

    Removed an Extra Print Statement

--- train.py
+++ train.py
@@ -90,7 +90,6 @@ def main(argv):
                         print(next(data.decode(fake)))
                         sess.close()
                         exit(0)
-                    print("Batch Num: ",batch_num)
                     if batch_num%print_freq == 0:
                         print("EPOCH: %d\tGENERATOR LOSS: %f\tDISCRIMINATOR LOSS: %f"%(_epoch,_gLoss,_dLoss))
                     l = next(data)
